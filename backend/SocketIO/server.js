e16780ed9b2b031c92d50cceca3998494a67bda4
tulsi-parmar1 <tulsiparmar878@gmail.com>
2025-08-07 15:32:39 +0530

    commit

--- backend/SocketIO/server.js
+++ backend/SocketIO/server.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://chat-app-411a.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true,
   },
