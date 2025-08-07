48908a67246bac9202b22d5350384f1ec6d43fe0
tulsi-parmar1 <tulsiparmar878@gmail.com>
2025-08-07 15:22:21 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -12,15 +12,15 @@ dotenev.config();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-    allowedHeaders: ["Content-Type"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
-
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//     allowedHeaders: ["Content-Type"],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//   })
+// );
+app.use(cors());
 const PORTNUMBER = process.env.PORT || 4001;
 
 try {
